42b59090d7aa2a7818917e999a4361dff104ab92
Rafael Alonso <rafalonso.almeida@gmail.com>
2021-08-26 13:36:50 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='lor_deckcodes',
-    version='3.0.0',
+    version='4.0.0',
     url='https://github.com/Rafalonso/LoRDeckCodesPython',
     description='Legends of Runeterra deck coder and decoder',
     long_description=open('README.md', 'r', encoding='utf-8').read(),
